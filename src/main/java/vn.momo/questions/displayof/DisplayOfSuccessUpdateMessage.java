fcb48921c4b3f92135e9a842114e44e5fed35f8a
BaoTran <tranquocbao14@gmail.com>
2021-01-06 00:56:39 +0700

    refactor code to verify result after upload avatar image

--- src/main/java/vn.momo/questions/displayof/DisplayOfSuccessUpdateMessage.java
+++ src/main/java/vn.momo/questions/displayof/DisplayOfSuccessUpdateMessage.java
@@ -3,15 +3,13 @@ package vn.momo.questions.displayof;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.annotations.Subject;
-import net.serenitybdd.screenplay.questions.Visibility;
 import vn.momo.ui.ProfileScreen;
 
 public class DisplayOfSuccessUpdateMessage implements Question<Boolean> {
     @Subject("display of successful update message")
     @Override
     public Boolean answeredBy(Actor theUser) {
-        return Visibility.of(ProfileScreen.SUCCESS_NOTIFICATION)
-                .viewedBy(theUser)
-                .resolve();
+        ProfileScreen.SUCCESS_NOTIFICATION.resolveFor(theUser).waitUntilVisible();
+        return true;
     }
 }
